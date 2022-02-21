a63dfadd28f6789dfaf942a88b509ce0e94858dc
Even Brenden <evenbrenden@gmail.com>
2022-02-21 19:04:42 +0100

    Update undefined.hs

--- chapter3/undefined.hs
+++ chapter3/undefined.hs
@@ -1,23 +1,3 @@
-addThree1 :: Int -> Int -> Int -> Int
-addThree1 x y z = x + y + z
-
-addThree2 :: Int -> Int -> Int -> Int
-addThree2 x y z = x + y + undefined
-
-addThree3 :: Int -> Int -> Int -> Int
-addThree3 x y z = x + undefined + z
-
-addThree4 :: Int -> Int -> Int -> Int
-addThree4 x y z = x + undefined + undefined
-
-addThree5 :: Int -> Int -> Int -> Int
-addThree5 x y z = undefined + y + z
-
-addThree6 :: Int -> Int -> Int -> Int
-addThree6 x y z = undefined + y + undefined
-
-addThree7 :: Int -> Int -> Int -> Int
-addThree7 x y z = undefined + undefined + z
-
-addThree8 :: Int -> Int -> Int -> Int
-addThree8 x y z = undefined + undefined + undefined
+-- Because undefined can represent any type, there are infinitely many variations.
+addThree :: Int -> Int -> Int -> Int
+addThree x y z = x `undefined` y `undefined` z
