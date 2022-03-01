1a52fc691f6ac6fd7b2d16a83c6d743852a0f67d
Even Brenden <evenbrenden@gmail.com>
2022-03-01 23:04:57 +0100

    Add traverse-directory.hs

---
+++ chapter10/traverse-directory.hs
@@ -0,0 +1,70 @@
+{-# LANGUAGE TypeApplications #-}
+
+import           Control.Exception              ( IOException
+                                                , handle
+                                                )
+import           Control.Monad                  ( join
+                                                , void
+                                                , when
+                                                )
+import           Data.Foldable                  ( for_ )
+import           Data.IORef
+import           Data.IORef                     ( modifyIORef
+                                                , newIORef
+                                                , readIORef
+                                                , writeIORef
+                                                )
+import           Data.List                      ( isSuffixOf )
+import qualified Data.Set                      as Set
+                                                ( empty
+                                                , insert
+                                                , member
+                                                )
+import           System.Directory               ( canonicalizePath
+                                                , doesDirectoryExist
+                                                , doesFileExist
+                                                , listDirectory
+                                                )
+
+data FileType
+    = FileTypeDirectory
+    | FileTypeRegularFile
+    | FileTypeOther
+
+classifyFile :: FilePath -> IO FileType
+classifyFile fname = do
+    isDirectory <- doesDirectoryExist fname
+    isFile      <- doesFileExist fname
+    pure $ case (isDirectory, isFile) of
+        (True , False) -> FileTypeDirectory
+        (False, True ) -> FileTypeRegularFile
+        _otherwise     -> FileTypeOther
+
+dropSuffix :: String -> String -> String
+dropSuffix suffix s | suffix `isSuffixOf` s = take (length s - length suffix) s
+                    | otherwise             = s
+
+traverseDirectory :: FilePath -> (FilePath -> IO ()) -> IO ()
+traverseDirectory rootPath action = do
+    seenRef <- newIORef Set.empty
+    let haveSeenDirectory canonicalPath =
+            Set.member canonicalPath <$> readIORef seenRef
+
+        addDirectoryToSeen canonicalPath =
+            modifyIORef seenRef $ Set.insert canonicalPath
+
+        traverseSubdirectory subdirPath = do
+            contents <- listDirectory subdirPath
+            for_ contents $ \file' -> handle @IOException (\_ -> pure ()) $ do
+                let file = subdirPath <> "/" <> file'
+                canonicalPath  <- canonicalizePath file
+                classification <- classifyFile canonicalPath
+                case classification of
+                    FileTypeOther       -> pure ()
+                    FileTypeRegularFile -> action file
+                    FileTypeDirectory   -> do
+                        alreadyProcessed <- haveSeenDirectory file
+                        when (not alreadyProcessed) $ do
+                            addDirectoryToSeen file
+                            traverseSubdirectory file
+    traverseSubdirectory (dropSuffix "/" rootPath)
