6be2bdb47bb7fc6111e0f4cc19f6e7d85ceeb648
Even Brenden <evenbrenden@gmail.com>
2022-11-20 20:26:17 +0100

    Add TypeFamilyListFuncs

---
+++ chapter15/TypeFamilyListFuncs.hs
@@ -0,0 +1,31 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE PolyKinds #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE UndecidableInstances #-}
+
+module TypeFamilyListFuncs where
+
+import Data.Kind
+import GHC.TypeLits
+
+type ReturnValue r = r -> Type
+
+data LessThanOrEqual :: Nat -> Nat -> ReturnValue Bool
+data Even :: Nat -> ReturnValue Bool
+
+type family Eval (expr :: ReturnValue r) :: r
+type instance Eval (LessThanOrEqual a b) = b <=? a
+type instance Eval (Even n) = EQ 0 (n `Mod` 2)
+
+type family EQ (a :: k) (b :: k) :: Bool where
+    EQ a a = True
+    EQ a b = False
+
+type family IfThenElse (p :: Bool) (t :: a) (f :: a) :: a where
+    IfThenElse True t _ = t
+    IfThenElse False _ f = f
+
+type family FindElems (p :: a -> ReturnValue Bool) (elems :: [a]) :: [a] where
+    FindElems _ '[] = '[]
+    FindElems p (a : as) = IfThenElse (Eval (p a)) (a : FindElems p as) (FindElems p as)
