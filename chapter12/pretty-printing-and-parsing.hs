7cb551845d7ad2e7ca0a7b57a253dc7536601d9e
Even Brenden <evenbrenden@gmail.com>
2023-03-15 19:27:30 +0100

    Add pretty printing exercise

--- chapter12/pretty-printing-and-parsing.hs
+++ chapter12/pretty-printing-and-parsing.hs
@@ -1,1 +1,7 @@
--- Pretty Printing and Parsing PASS
+-- Pretty Printing and Parsing
+
+-- Prettifier for a text format defined as words that are separated by one or
+-- more whitespaces, prettified to contain only one whitespace in between the
+-- words ¯\_(ツ)_/¯
+prettify :: String -> String
+prettify = unwords . words
